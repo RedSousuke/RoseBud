5f007ef6e8d74bd4943a95ff4f76263f5b160656
RedSousuke <rmeeks@student.neumont.edu>
2025-02-26 11:10:25 -0700

    minor changes to window. added base file for keras speech to text

--- RoseBud/RoseBud.pyw
+++ RoseBud/RoseBud.pyw
@@ -1,3 +1,4 @@
 import RBWindow
 
+
 RBWindow.start()
---
+++ RoseBud/kerasfortranscription.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import pandas as pd
+import  matplotlib.pyplot as plt
+import numpy as np
+from IPython import display
+from jiwer import wer
+
+
+# Following code example at https://keras.io/examples/audio/ctc_asr/
+
+data_url = "https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2"
+data_path = keras.utils.get_file("LJSpeech-1.1", data_url, untar=True)
+wavs_path = data_path + "/wavs/"
+metadata_path = data_path + "/metadata.csv"
+
+
+# Read metadata file and parse it
+metadata_df = pd.read_csv(metadata_path, sep="|", header=None, quoting=3)
+metadata_df.columns = ["file_name", "transcription", "normalized_transcription"]
+metadata_df = metadata_df[["file_name", "normalized_transcription"]]
+metadata_df = metadata_df.sample(frac=1).reset_index(drop=True)
+metadata_df.head(3)
+
